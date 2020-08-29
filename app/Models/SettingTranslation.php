010c10c9ebd254eb3fee38dfebec9beee8c6529e
Ibrahim AbdelBaki <3baa2y@gmail.com>
2020-08-29 19:46:15 +0200

    =end first branch

---
+++ app/Models/SettingTranslation.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SettingTranslation extends Model
+{
+    protected $fillable=['value'];
+
+    public $timestamps=false;
+}
